0006ffe3a634e94517f7cc93a25f5949b58a2ae5
Celin Villars <villarscelin@gmail.com>
2024-07-12 23:36:50 +0200

    update documentation workflow

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"app.src"},{"l":"app.src.core"},{"l":"app.src.resources"},{"l":"app.src.resources.assets"},{"l":"app.src.resources.components"},{"l":"app.src.resources.monsters"},{"l":"app.src.scenes"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"}];updateSearchResults();
